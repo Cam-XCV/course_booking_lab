ba92b0720e0e2a8287edb451918772a9572a7953
cam <campbellmurray0@gmail.com>
2020-11-26 10:24:14 +0000

    first commit - initial setup

---
+++ src/test/java/com/example/codeclan/bookingsystem/BookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
